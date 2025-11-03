25b4c7207b928f59f4fe06948ea4dfbc3c34129c
Ngozi-N <jonathanengee@gmail.com>
2025-11-03 15:42:35 +0100

    Update on BankappApplication.java file

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -3,11 +3,9 @@ package com.example.bankapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.bankapp", "com.engee.bank"})
 public class BankappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BankappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BankappApplication.class, args);
+    }
 }
